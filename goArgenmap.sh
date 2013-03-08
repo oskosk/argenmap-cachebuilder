afb70b8043dfd63b121359a31b907883d521d90d
Oscar Lopez <oskosk@gmail.com>
2013-03-08 11:52:15 -0300

    cambié el orden de los parámetros

--- goArgenmap.sh
+++ goArgenmap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 date
-python ./tilecache_seed.py -f capabaseargenmap 0 31 --bbox=-20037508.34,-20037508.34,0,0
+python ./tilecache_seed.py  --bbox=-20037508.34,-20037508.34,0,0 capabaseargenmap 0 31
 date
