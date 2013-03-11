7bc7336c48f7599c332b430741dfcffd4c2c02de
Oscar Lopez <oskosk@gmail.com>
2013-03-11 18:13:02 -0300

    cambié bbox porque era mucho el bbox anterior. lo limito al bbox de la capa provincias_lim en coord mercator

--- goArgenmap.sh
+++ goArgenmap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+BBOX=-20037508.34,-20037508.34,0,0
+BBOX=-8240965.645900,-30240971.958400,-2785563.741300,-2484886.405700
 date
-python ./tilecache_seed.py  --bbox=-20037508.34,-20037508.34,0,0 capabaseargenmap 0 31
+python ./tilecache_seed.py  --bbox=$BBOX capabaseargenmap 0 31
 date
