9f9f06cd48aed405ef691801a07b0cb53143662a
Oscar Lopez <oskosk@gmail.com>
2013-03-19 22:17:33 -0300

    agregué BBOX de la antártida como ejemplo comentado

--- goArgenmap.sh
+++ goArgenmap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-BBOX=-20037508.34,-20037508.34,0,0
+# provincias_lim
 BBOX=-8240965.645900,-30240971.958400,-2785563.741300,-2484886.405700
+#Antártida
+#BBOX=-8237642.317555556,-59613540.1989548,-2782987.2694,-8399737.888649108
 date
 python ./tilecache_seed.py  --bbox=$BBOX capabaseargenmap 0 31
 date
