7d7629b5176f55d0818c92cbe292f8454818d870
oskosk <oskosk@gmail.com>
2013-03-22 04:21:07 -0300

    cambié la manera de representar las conexiones para los grafos porque ahora hay más de un nodo de caché

--- php-frontend/consola/ping.php
+++ php-frontend/consola/ping.php
@@ -1,6 +1,6 @@
 <?php
 $logdir = dirname(dirname(__FILE__)) . "/tms/logs";
-$NLINES = 1000;
+$NLINES = 50;
 $cmd = "tail -n $NLINES $logdir/log.txt";
 
 //echo ('a '. microtime() .  "<br/>");
@@ -33,21 +33,26 @@ foreach ($lines as $ll) {
 	}
 	$tein_proxy = ($private_ip != 'IP privada N/D');
 	$thisNode = "http://www.ign.gob.ar/tms";
-  if ($referer) {
-  	if ($tein_proxy) {
-  		$uniq[] =  "$referer -> Proxy $ip  {color:#218559, weight:2}";	
-  	} else {
-  		$uniq[] =  "$referer -> $ip  {color:#218559, weight:2}";	
-  	}
+  	if ($referer) {
+  		if ($tein_proxy) {
+  			$uniq[] = "$referer -> Proxy $ip  {color:#218559, weight:2}";	
+			$uniq[] = "$thisNode -> Proxy $ip {color:#192823, weight:5}";
+			$uniq[] = "Proxy $ip -> $private_ip  {color:#D0C6B1, weight:2}";
+  		} else {
+  			$uniq[] = "$referer -> $ip  {color:#218559, weight:2}";	
+			$uniq[] = "$thisNode -> $ip {color:#192823, weight:5}";
+  		}
 		
-		$uniq[] = "$thisNode -> $referer {color:#192823, weight:5}";
 		$uniqnodes[] = "$referer {color:#218559} ";
 	} else {
-		$uniq[] =  "$ip";
-		$uniq[] = "$thisNode -> $ip {color:red, weight:5}";
+		if ($tein_proxy) {
+			$uniq[] = "Proxy $ip -> $private_ip {color:red, weight:2}";
+			$uniq[] = "$thisNode -> Proxy $ip {color:red, weight:5}";
+		} else {
+			$uniq[] = "$thisNode -> $ip {color:red, weight:5}";
+		}
 	}
 	if ($tein_proxy) {
-		$uniq[] = "Proxy $ip -> $private_ip  {color:#D0C6B1, weight:2}";
 		$uniqnodes[] = "Proxy $ip {color:#D0C6B1}";
 		$uniqnodes[] = "$private_ip {color:#EBB035}";
 	} else {
@@ -59,4 +64,8 @@ foreach ($lines as $ll) {
 	echo implode("\n", array_unique($uniq)); 
 	echo "\n";
 	echo implode("\n", array_unique($uniqnodes)); 
+	echo "\n";
+	echo file_get_contents('http://sig.ign.gob.ar/consola/ping.php');
+	echo "\n";
+	echo file_get_contents('http://190.220.8.216/consola/ping.php');
 
