54502d4544620459c813ea57e12667c7e08f1e42
oskosk <oskosk@gmail.com>
2013-03-23 15:02:39 -0300

    ahora incluyo los este_nodo_ping.php remotos en lugar de los ping.php remotos

--- php-frontend/consola/ping.php
+++ php-frontend/consola/ping.php
@@ -1,7 +1,7 @@
 <?php
 	include('este_nodo_ping.php');
 	echo "\n";
-	echo file_get_contents('http://sig.ign.gob.ar/consola/ping.php');
+	echo file_get_contents('http://sig.ign.gob.ar/consola/este_nodo_ping.php');
 	echo "\n";
-	echo file_get_contents('http://190.220.8.216/consola/ping.php');
+	echo file_get_contents('http://190.220.8.216/consola/est_nodo_ping.php');
 
