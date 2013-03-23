a956b8ba931e45eede54828d5977b0e89f232f3e
oskosk <oskosk@gmail.com>
2013-03-23 17:47:09 -0300

    typo en referencia a la ruta del script este_nodo_ping.php

--- php-frontend/consola/ping.php
+++ php-frontend/consola/ping.php
@@ -3,5 +3,5 @@
 	echo "\n";
 	echo file_get_contents('http://sig.ign.gob.ar/consola/este_nodo_ping.php');
 	echo "\n";
-	echo file_get_contents('http://190.220.8.216/consola/est_nodo_ping.php');
+	echo file_get_contents('http://190.220.8.216/consola/este_nodo_ping.php');
 
