79343fe1a153529e3f07a1e8f36766004518976d
Oscar Lopez <oskosk@gmail.com>
2013-03-26 20:52:00 -0300

    varios cambios a api/index.php por el cambio de formato de log

--- php-frontend/consola/api/index.php
+++ php-frontend/consola/api/index.php
@@ -10,21 +10,115 @@ $app = new Slim( array(
 
 $app->get('/stats/ping', 'ping');
 $app->get('/stats/este_nodo_ping', 'este_nodo_ping');
+$app->get('/stats/estenodo/:datetime/requests/referers', 'requests_por_datetime_referers_este_nodo');
+$app->get('/stats/estenodo/:datetime/requests/resumen', 'requests_resumen_por_datetime_este_nodo');
+$app->get('/stats/estenodo/:datetime/requests', 'requests_por_datetime_este_nodo');
+$app->get('/stats/estenodo/ultimosrequests', 'ultimos_requests_este_nodo');
+$app->get('/stats/ultimosrequests', 'ultimos_requests');
+
 
 
 function ping() {
   global $CONFIG;
   
   //include('../este_nodo_ping.php');
-  foreach ($CONFIG['otros_nodos_url'] as $url) {
-    echo "\n";    
-    echo file_get_contents($url . '/consola/api/este_nodo_ping');
+  $relaciones = array();
+  $nodos = array();
+  $nodos_url = $CONFIG['otros_nodos_url'];
+  $nodos_Url[] = $CONFIG['este_nodo_url'];
+  foreach ($nodos_url as $url) {
+    $a = json_decode(file_get_contents($url . '/consola/api/stats/este_nodo_ping'),true);
+    $relaciones = array_merge($relaciones, $a['relaciones']);
+    $nodos = array_merge($nodos, $a['nodos']);
+
   }
+    echo json_encode($relaciones);
+    echo json_encode($nodos);
+}
+
+function ultimos_requests() {
+  global $CONFIG;
+  
+  //include('../este_nodo_ping.php');
+  $requests = array();
+  $nodos = array();
+  $nodos_url = $CONFIG['otros_nodos_url'];
+  $nodos_url[] = $CONFIG['este_nodo_url'];
+  foreach ($nodos_url as $url) {
+    $a = json_decode(file_get_contents($url . '/consola/api/stats/estenodo/ultimosrequests'),true);
+    $requests = array_merge($relaciones, $a['requests']);
+
+  }
+    echo json_encode($relaciones);
+}
+
+function requests_por_datetime_este_nodo($datetime)
+{
+  global $CONFIG;
+
+  $thisNode = $CONFIG['este_nodo_url'];
+  $stats = new ArgenmapCacheStats();
+  $lines = $stats->requestsPorDateTime($datetime);
+  
+  $ret = array();
+
+  $ret['este_nodo'] = $thisNode;
+  $ret['count'] = count($lines);
+  $ret['consumo_estimado'] = 8 * count($lines) . ' KB';
+  $ret['requests'] = $lines; 
+  
+  echo json_encode($ret);  
+  return json_encode($ret);
 
 }
 
+function requests_resumen_por_datetime_este_nodo($datetime)
+{
+  global $CONFIG;
 
-function este_nodo_ping()
+  $thisNode = $CONFIG['este_nodo_url'];
+  $stats = new ArgenmapCacheStats();
+
+  $lines = $stats->requestsPorDateTime($datetime);
+  $clientes = $stats->clientesPorDateTime($datetime);
+
+  $ret = array();
+  $ret['este_nodo'] = $thisNode;
+  $ret['count'] = count($lines);
+  $ret['count_clientes'] = count($clientes);
+  //Asumo que cada tile pesa 8KB
+  //Tengo quecalcular un promedio real en base
+  // a los requests reales.
+  $ret['consumo_estimado'] = 8 * count($lines) . ' KB';
+  $ret['segundos'] = $stats->_segundosTotalesPorDateTime($datetime);  
+
+  $ret['ancho_de_banda_estimado'] = ( ( 8 * count($lines) ) /  $ret['segundos']). ' KB/S';
+    
+  echo json_encode($ret);  
+  return json_encode($ret);
+
+}
+
+function requests_por_datetime_referers_este_nodo($datetime)
+{
+  global $CONFIG;
+
+  $thisNode = $CONFIG['este_nodo_url'];
+  $stats = new ArgenmapCacheStats();
+  $lines = $stats->referersPorDateTime($datetime);
+  
+  $ret = array();
+
+  $ret['este_nodo'] = $thisNode;
+  $ret['count'] = count($lines);
+  $ret['referers'] = $lines; 
+  
+  echo json_encode($ret);  
+  return json_encode($ret);
+
+}
+
+function ultimos_requests_este_nodo()
 {
   global $CONFIG;
 
@@ -32,7 +126,16 @@ function este_nodo_ping()
   $stats = new ArgenmapCacheStats();
   $lines = $stats->ultimosRequests();
   $uniq = array();
-  $uniqnodes = array();
+  $uniq_referers = array();
+  
+  $ret = array();
+
+  $ret['este_nodo'] = $thisNode;
+  $ret['requests'] = $lines; 
+  
+  echo json_encode($ret);  
+  return json_encode($ret);
+
   foreach ($lines as $ll) {
     $referer = $ll['client']['referer'];
     $ip = $ll['client']['ip'];
@@ -71,11 +174,7 @@ function este_nodo_ping()
     }
 
   }
-    $uniqnodes[] = "$thisNode {color:#192823}";
-    $ret = array();
-    $ret['relaciones'] = array_unique($uniq); 
-    $ret['nodos'] = array_unique($uniqnodes); 
-    echo json_encode($ret);
+
 }
 
 $app->run();
