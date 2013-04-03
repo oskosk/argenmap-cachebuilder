9068dbc633bc7db5dcb26449d0c0c82808873b07
Oscar Lopez <oskosk@gmail.com>
2013-04-03 17:41:28 -0300

    saqué endpoints que no servían y moví funciones a metodos/stats.php

--- php-frontend/consola/api/index.php
+++ php-frontend/consola/api/index.php
@@ -4,37 +4,21 @@ require "../../config.inc.php";
 require "../argenmap_cache_stats.php";
 require "Slim/Slim.php";
 
+require "metodos/stats.php";
+
 $app = new Slim( array(
   'debug'=>true
 ));
 
-$app->get('/stats/ping', 'ping');
-$app->get('/stats/este_nodo_ping', 'este_nodo_ping');
-$app->get('/stats/estenodo/:date/requests/referers', 'requests_por_date_referers_este_nodo');
-$app->get('/stats/estenodo/:date/requests', 'requests_resumen_por_date_este_nodo');
+$app->get('/stats/ping', 'stats_ping');
+$app->get('/stats/estenodo/:date/requests', 'stats_este_nodo_requests_por_date');
 $app->get('/stats/estenodo/requests', 'requests_este_nodo');
 $app->get('/stats/estenodo/ultimosrequests', 'ultimos_requests_este_nodo');
 $app->get('/stats/ultimosrequests', 'ultimos_requests');
 
 
 
-function ping() {
-  global $CONFIG;
-  
-  //include('../este_nodo_ping.php');
-  $relaciones = array();
-  $nodos = array();
-  $nodos_url = $CONFIG['otros_nodos_url'];
-  $nodos_Url[] = $CONFIG['este_nodo_url'];
-  foreach ($nodos_url as $url) {
-    $a = json_decode(file_get_contents($url . '/consola/api/stats/este_nodo_ping'),true);
-    $relaciones = array_merge($relaciones, $a['relaciones']);
-    $nodos = array_merge($nodos, $a['nodos']);
 
-  }
-    echo json_encode($relaciones);
-    echo json_encode($nodos);
-}
 
 function ultimos_requests() {
   global $CONFIG;
@@ -80,33 +64,6 @@ function requests_este_nodo()
     $retAll[] = $ret;  
   } 
   echo  json_encode($retAll);
-}
-
-function requests_resumen_por_date_este_nodo($date)
-{
-  global $CONFIG;
-
-  $thisNode = $CONFIG['este_nodo_url'];
-  $stats = new ArgenmapCacheStats();
-
-  $lines = $stats->requestsPorDate($date);
-  $clientes = $stats->clientesPorDate($date);
-
-  $ret = array();
-  $ret['este_nodo'] = $thisNode;
-  $ret['count'] = count($lines);
-  $ret['count_clientes'] = count($clientes);
-  //Asumo que cada tile pesa 8KB
-  //Tengo quecalcular un promedio real en base
-  // a los requests reales.
-  $ret['consumo_estimado'] = 8 * count($lines) . ' KB';
-  $ret['segundos'] = $stats->_segundosTotalesPorDate($date);  
-
-  $ret['ancho_de_banda_estimado'] = ( ( 8 * count($lines) ) /  $ret['segundos']). ' KB/S';
-    
-  echo json_encode($ret);  
-  return json_encode($ret);
-
 }
 
 function requests_por_datetime_referers_este_nodo($datetime)
