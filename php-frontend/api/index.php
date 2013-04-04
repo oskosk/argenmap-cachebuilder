740614e2dfc84734aee131e714e385ed6e7483eb
Oscar Lopez <oskosk@gmail.com>
2013-04-03 22:26:21 -0300

    saqué línea repetida

--- php-frontend/api/index.php
+++ php-frontend/api/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require "../../config.inc.php";
-require "../argenmap_cache_stats.php";
+require "../config.inc.php";
+require "argenmap_cache_stats.php";
 require "Slim/Slim.php";
 
 require "metodos/stats.php";
@@ -14,7 +14,6 @@ $app->get('/stats/ping', 'stats_ping');
 $app->get('/stats/estenodo/:date/requests', 'stats_este_nodo_requests_por_date');
 $app->get('/stats/estenodo/requests', 'stats_estenodo_requests');
 $app->get('/stats/estenodo/ultimosrequests', 'stats_estenodo_ultimos_requests');
-$app->get('/stats/ultimosrequests', 'stats_ultimos_requests');
 $app->get('/stats/ultimosrequests', 'stats_ultimos_requests');
 
 $app->run();
