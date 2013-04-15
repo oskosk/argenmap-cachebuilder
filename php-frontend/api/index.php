ffd19b4ebf1c515efc7d47efd40f475a2f43dd63
Oscar Lopez <oskosk@gmail.com>
2013-04-15 18:20:05 -0300

    Closes #8. Quité el endpoint stats/ping

--- php-frontend/api/index.php
+++ php-frontend/api/index.php
@@ -13,7 +13,6 @@ $app = new Slim( array(
   'debug'=>true
 ));
 
-$app->get('/stats/ping', 'stats_ping');
 $app->get('/stats/estenodo/:date/requests', 'stats_este_nodo_requests_por_date');
 $app->get('/stats/estenodo/requests', 'stats_estenodo_requests');
 $app->get('/stats/estenodo/ultimosrequests', 'stats_estenodo_ultimos_requests');
