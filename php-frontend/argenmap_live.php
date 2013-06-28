cc5313e4b55401d7e7ae7330c931e404439b0647
Christian Gastrell <cg@ubuntu-vm.(none)>
2013-06-28 16:19:26 -0300

    update para argenmap_live segun cg

--- php-frontend/argenmap_live.php
+++ php-frontend/argenmap_live.php
@@ -1,79 +1,239 @@
 <?php
+$usarSesion = isset($_GET['polling']) && $_GET['polling'] == true;
 header("Access-Control-Allow-Origin: *");
-header('Content-Type: text/event-stream');
-header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
-/**
- * Constructs the SSE data format and flushes that data to the client.
- *
- * @param string $id Timestamp/id of this connection.
- * @param string $msg Line of text that should be transmitted.
- */
-function _sendMsg($id, $msg) {
-        echo "id: $id" . PHP_EOL;
-        //echo "data: {" . PHP_EOL;
-        echo "data: $msg" . PHP_EOL;
-        //echo "data: }" . PHP_EOL;
-        echo PHP_EOL;
-        ob_flush();
-        flush();
+if($usarSesion)
+{
+	header('Content-Type: text/text');
+}else{
+	header('Content-Type: text/event-stream');
 }
+header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
 
 $serverTime = time();
-$live = new ArgenmapLive("tms/logs/log.txt");
-$live->getNewLines();
-_sendMsg($serverTime, count($live->LOG_DIFF_LINES));
-//echo "<pre>";
-//print_r($live);
-//echo "</pre>";
+$live = new ArgenmapLive("tms/logs/log.txt",$usarSesion);
+
+if($usarSesion)
+{
+	echo "data: " . json_encode($live->results) . " " . PHP_EOL;
+	echo PHP_EOL;
+	ob_flush();
+	flush();
+}else{
+	//ver este ejemplo de implementacion php de sse
+	//https://developer.mozilla.org/en-US/docs/Server-sent_events/Using_server-sent_events
+	//se puede hacer sin session con el while, creo
+	while(1) {
+		echo "event: ping\n";
+		$live->procesar();
+		echo 'data: ' . json_encode($live->results) . " \n";
+		echo "\n\n";
+		ob_flush();
+		flush();
+		sleep(2);
+	}
+}
 class ArgenmapLive
 {
-        private $SID;
-        private $LOG_FILE;
-        private $FROM_LINE = 0;
-        public $LOG_LINE_COUNT;
-        public $LOG_DIFF_LINES;
+	private $LOG_LINE_COUNT;
+	private $LOG_FILE_PATH;
+	private $FROM_LINE = 0;
+	private $START_MICRO_TIME;
+	private $error = false;
+	private $errorTxt = "OK";
+	private $USE_SESSION = true;
+	private $numDiffLines;
+	private $rawDiffLines;
+	private $indexedDiffLines;
+	public $results;
 
-        function __construct($filePath)
-        {
-                $this->LOG_FILE = realpath($filePath);
-                $this->countLogLines();
-                $this->initializeSession();
-        }
-        private function initializeSession()
-        {
-                session_start();
-                $this->SID = session_id();
-                if(!isset($_SESSION["argenmap_stats"]))
-                {
-                        $_SESSION["argenmap_stats"] = array();
-                        $_SESSION["argenmap_stats"]["from_line"] = $this->LOG_LINE_COUNT;
-                }
-                $this->FROM_LINE = $_SESSION["argenmap_stats"]["from_line"];
-        }
-        function __destruct()
-        {
-                $_SESSION["argenmap_stats"]["from_line"] = $this->LOG_LINE_COUNT;
-        }
-        private function countDiffLines()
-        {
-                return $this->LOG_LINE_COUNT - $this->FROM_LINE;
-        }
-        private function countLogLines()
-        {
-                $wc = shell_exec("wc -l $this->LOG_FILE");
-                $r = explode(" ", $wc);
-                $this->LOG_LINE_COUNT = $r[0];
-        }
-        public function getNewLines()
-        {
-                if($this->countDiffLines() == 0)
-                {
-                        $this->LOG_DIFF_LINES = array();
-                        return $this->LOG_DIFF_LINES;
-                }
-                $cmd = "tail -n {$this->countDiffLines()} $this->LOG_FILE";
-                $tail = shell_exec($cmd);
-                $this->LOG_DIFF_LINES = explode("\n", $tail);
-                return $this->LOG_DIFF_LINES;
-        }
+	function __construct($filePath, $useSession = true)
+	{
+		$this->USE_SESSION = $useSession;
+		$this->START_MICRO_TIME = microtime(true);
+		$this->resetVars();
+		if($_GET['sid']) session_id($_GET['sid']);
+		session_start();
+		if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 180))
+		{
+			// last request was more than 30 minutes ago
+			session_unset();     // unset $_SESSION variable for the run-time 
+			session_destroy();   // destroy session data in storage
+		}
+		$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+		$this->LOG_FILE_PATH = realpath($filePath);
+		if(!$this->LOG_FILE_PATH) 
+		{
+			$this->setError("No se encuentra el log. ({$filePath})");
+			return;
+		}
+		$wc = shell_exec("wc -l $this->LOG_FILE_PATH");
+		$r = explode(" ", $wc);
+		$this->LOG_LINE_COUNT = (int) $r[0];
+		if(!isset($_SESSION["argenmap_stats"]))
+		{
+			$_SESSION["argenmap_stats"] = array();
+			$_SESSION["argenmap_stats"]["leerDesde"] = $this->LOG_LINE_COUNT;
+		}
+		$this->FROM_LINE = $_SESSION["argenmap_stats"]["leerDesde"];
+		$this->countDiffLines();
+		$this->getRawDiffLines();
+		$this->indexLogLines();
+		$this->consolidarResultados();
+	}
+	function __destruct()
+	{
+		$_SESSION["argenmap_stats"]["leerDesde"] = $this->LOG_LINE_COUNT;
+	}
+	private function resetVars()
+	{
+		$this->numDiffLines = 0;
+		$this->rawDiffLines = array();
+		$this->indexedDiffLines = array();
+		$this->results = array(
+			'requestsTotales' => 0,
+			'requestsNuevos' => 0,
+			'requestsCrudos' => array(),
+			'requestsIndexados' => array(),
+			'processData' => array(
+				'status' => 'OK',//o success, no se
+				'error' => false,
+				'serverTime' => time(),
+				'processMicrotime' => 0
+			)
+		);
+	}
+	private function setError($txt)
+	{
+		$this->error = true;
+		$this->errorTxt = $txt;
+		$this->consolidarResultados();
+	}
+	public function procesar()
+	{
+		$this->START_MICRO_TIME = microtime(true);
+		$this->resetVars();
+		
+		$wc = shell_exec("wc -l $this->LOG_FILE_PATH");
+		$r = explode(" ", $wc);
+		$this->LOG_LINE_COUNT = (int) $r[0];
+		$this->countDiffLines();
+		$this->getRawDiffLines();
+		$this->indexLogLines();
+		$this->consolidarResultados();
+	}
+	private function consolidarResultados()
+	{
+		$this->results['requestsTotales'] = $this->LOG_LINE_COUNT;
+		$this->results['requestsNuevos'] = $this->numDiffLines;
+		$this->results['requestsCrudos'] = $this->rawDiffLines;
+		$this->results['requestsIndexados'] = $this->indexedDiffLines;
+		$this->results['processData']['processMicrotime'] = microtime(true) - $this->START_MICRO_TIME;
+		$this->results['processData']['error'] = $this->error;
+		$this->results['processData']['status'] = $this->errorTxt;
+		$this->results['processData']['sessionId'] = session_id();
+		$this->FROM_LINE = $this->LOG_LINE_COUNT;
+	}
+	private function countDiffLines()
+	{
+		$this->numDiffLines = $this->LOG_LINE_COUNT - $this->FROM_LINE;
+	}
+	public function getRawDiffLines()
+	{
+		if($this->numDiffLines < 1)
+		{
+			return false;
+		}
+		$cmd = "tail -n {$this->numDiffLines} $this->LOG_FILE_PATH";
+		$tail = shell_exec($cmd);
+		$this->rawDiffLines = explode("\n", $tail);
+		for($ii = 0; $ii < count($this->rawDiffLines); $ii++)
+		{
+			if(!$this->rawDiffLines[$ii]) unset($this->rawDiffLines[$ii]);
+		}
+	}
+	private function indexLogLines()
+	{
+		$lines = &$this->rawDiffLines;
+		$this->indexedDiffLines = array(
+			'porReferer' => array(),
+			'porIP' => array(),
+			'porDate' => array(),
+			'porDateTime' => array()			
+		);
+
+		if (count($lines) == 0 ) {
+			return;
+		}		
+		foreach($lines as &$ll) {
+			if ($ll == '') {
+				continue;
+			}
+			$request = $this->_parseLine($ll);
+			
+			$date = $request['date'];
+			$datetime = $request['datetime'];
+			$referer = $request['referer'];
+			$ip = $request['ip'];
+			$private_ip = $request['private_ip'];
+
+			$this->indexedDiffLines['porReferer'][$referer][] = &$ll;
+			$this->indexedDiffLines['porIP'][$ip][] = &$ll;
+			$this->indexedDiffLines['porDate'][$date][] = &$ll;
+			$this->indexedDiffLines['porDateTime'][$datetime][] = &$ll;
+		}
+	}
+	protected function _parseLine(&$line) 
+	{
+		if (trim($line) == '') {
+			return false;
+		}
+		
+		$request = explode("\t", $line);
+		$trash = explode(' - ', $request[0]);
+		$datetime = $trash[0];
+		$trash = explode(' ', $datetime);
+		$date = $trash[0];
+		$referer = $request[4];
+		$ip = $request[5];
+		//este campo suele venir vacío porque
+		// generalmente no hay proxies involucrador en el request
+		// De hecho, muchos proxies ocultan la ip privada		
+		$private_ip = @$request[6];
+
+		$ret = array();
+		$ret['date'] = $date;
+		$ret['datetime'] = $datetime;
+		$ret['tile'] = array(
+			'z'=>$request[1],
+			'x'=>$request[2],
+			'y'=>$request[3]
+		);
+		$ret['referer'] = $referer;
+		
+		// Este chequeo es porque el proxy de AppFog, 
+		// en el header(string) X_FORWARDED_FOR mete
+		// la IP pública del cliente y un 127.0.0.1
+		// separados por comas porque usa reverse
+		// proxies para balancear los pedidos a cada app
+		$private_stuff = explode(',', $private_ip);
+		if ( count($private_stuff) == 2) {
+			//caso de request normal sin proxy
+			$ip = $private_stuff[0];
+			$private_ip = false;
+		} elseif( count($private_stuff) == 3) {
+			//caso de request normal con proxy
+			$ip = $private_stuff[1];
+			$private_ip = $private_stuff[0];
+		} elseif( count($private_stuff) == 4) {
+			// caso de request normal con proxy pero
+			// con dos 127.0.0.1 en el campo x_forwarded_for
+			$ip = $private_stuff[2];
+			$private_ip = $private_stuff[0];
+		}
+		$ret['ip'] = trim($ip);		
+		$ret['private_ip'] = trim($private_ip);
+
+		return $ret;
+	}
+
 }
