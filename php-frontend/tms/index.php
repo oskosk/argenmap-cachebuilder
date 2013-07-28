9ff7111e3c0ad00afd2aee4f4864cd99d10846a2
osk <osk@li316-77.members.linode.com>
2013-07-28 20:56:53 -0300

    cambios en tms/idnex.php cambié paths de dir de cache, cambie requires por require_once. meti el codigo en una gran funcion para que se llamada desde el handler de slim, quite el parseo de url, porque ahora lo maneja slim y esta gran funcion ahora devuelve null si fallo traerTile o la cantidad de bytes que pesa la tile si traerTile funciona

--- php-frontend/tms/index.php
+++ php-frontend/tms/index.php
@@ -1,45 +1,32 @@
 <?php
-
-require('KLogger.php');
-require('cache.php');
-require('argenmap_cache.php');
+function tms($capa,$z,$x,$y,$format)
+{
+require_once('KLogger.php');
+require_once('cache.php');
+require_once('argenmap_cache.php');
 
 // un año en segundos
 define('CACHE_TTL',  31536000);
 
-$cache = new Argenmap_Cache('./cache');
+$cache = new Argenmap_Cache('tms/cache');
 
-
-$q= @$_GET['q']; 
 
 $baseURL = 'http://mapa.ign.gob.ar/geoserver/gwc/service/tms/1.0.0';
 $tileURL = $baseURL;
 
-$pieces = explode('/', $q);
-if (count($pieces) != 4) {
-	$cache->LogError("\tMALA_URL\t$_SERVER[QUERY_STRING]\t");
-	die('hard');
-}
-
-$capa = $pieces[0] . '@EPSG:3857@png8';
-$z = $pieces[1];
-$x = $pieces[2];
-$y = $pieces[3]; 
-$y = explode('.', $y);
-$format = $y[1];
-$y=$y[0];
+$capa = $capa . '@EPSG:3857@png8';
 //apendeo el 8 para pedir png8
 $tileURL = sprintf("%s/%s/%s/%s/%s.%s", $baseURL, $capa, $z, $x, $y,$format."8");
 
-
-if ($cache->traerTile($tileURL) ) {
+$bytes_sent = NULL;
+if ($bytes_sent = $cache->traerTile($tileURL) ) {
 	$cache->logTileServida($z, $x, $y);
 } else {
-			$cache->LogError("\tNo se pudo leer la tile desde el servicio TMS remoto:\t%s\t%x\%y", $z, $x, $y);	
+	$cache->LogError("\tNo se pudo leer la tile desde el servicio TMS remoto:\t%s\t%x\%y", $z, $x, $y);	
 }
 
-
-
+	return $bytes_sent;
+}
 
 
 
