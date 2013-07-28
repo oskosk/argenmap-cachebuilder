444f147ce6bcde7aa37806d5f8e85284b290f1f0
osk <osk@li316-77.members.linode.com>
2013-07-28 19:58:25 -0300

    hice copia de api/index.php en root de php-frontend

---
+++ php-frontend/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require "../config.inc.php";
+require "argenmap_cache_stats.php";
+require "Slim/Slim.php";
+
+require "metodos/auth.php";
+require "metodos/stats.php";
+require "metodos/cache.php";
+require "metodos/status.php";
+
+
+$app = new Slim( array(
+  'debug'=>true
+));
+
+$app->get('/stats/estenodo/:date/requests', 'stats_este_nodo_requests_por_date');
+$app->get('/stats/estenodo/requests', 'stats_estenodo_requests');
+$app->get('/stats/estenodo/ultimosrequests', 'stats_estenodo_ultimos_requests');
+$app->get('/stats/ultimosrequests', 'stats_ultimos_requests');
+
+$app->get('/cache/truncate', 'cache_truncate');
+$app->get('/cache/estenodo/truncate', 'auth_admin', 'cache_estenodo_truncate');
+
+$app->get('/status/estenodo', 'status_estenodo');
+$app->run();
+
