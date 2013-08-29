97a91dd570139675b9161f5e7fd8c46c5275d62e
osk <osk@li316-77.members.linode.com>
2013-08-28 21:05:25 -0300

    aseguré con auth los metodos de logs y cache

--- php-frontend/index.php
+++ php-frontend/index.php
@@ -22,10 +22,10 @@ $app->get('/tms/:capa/:z/:y/:x\.:format/status.json', 'Controllers\TMS::status')
 $app->get('/cache/truncate', '\Argenmap\Auth::isAdmin', '\Controllers\Cache::truncate');
 $app->get('/cache/status.json', '\Argenmap\Auth::isAdmin', '\Controllers\Cache::status');
 
-$app->get('/logs/ultimosrequests.json', 'Controllers\Logs::ultimos_requests');
-$app->get('/logs/disponibles.json', 'Controllers\Logs::logs_disponibles');
-$app->get('/logs/:date/requests.json', 'Controllers\Logs::requests_por_date');
-$app->get('/logs/:date/errors.json', 'Controllers\Logs::errors_por_date');
+$app->get('/logs/ultimosrequests.json', '\Argenmap\Auth::isAdmin', 'Controllers\Logs::ultimos_requests');
+$app->get('/logs/disponibles.json', '\Argenmap\Auth::isAdmin', 'Controllers\Logs::logs_disponibles');
+$app->get('/logs/:date/requests.json', '\Argenmap\Auth::isAdmin', 'Controllers\Logs::requests_por_date');
+$app->get('/logs/:date/errors.json', '\Argenmap\Auth::isAdmin', 'Controllers\Logs::errors_por_date');
 
 $app->get('/live/poll', '\Controllers\Live::live_poll');
 $app->get('/live', '\Controllers\Live::live');
